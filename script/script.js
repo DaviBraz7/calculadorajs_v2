8fa0cd700526ad659fae502ff6eefb25f3694e71
Davi Braz <davisouzabraz@gmail.com>
2022-04-16 00:49:10 -0300

    atualização de projeto

--- script/script.js
+++ script/script.js
@@ -1,7 +1,21 @@
 function displaynum(n1){
     var num = document.getElementById("result").innerHTML
     document.getElementById("result").innerHTML = num + n1 ;
+
 }
+
+
+function funcion(p1) {
+    calculate();
+    var num = document.getElementById("result").innerHTML
+    document.getElementById("result").innerHTML = num + p1      
+}
+
+function back(){
+    var result = document.getElementById("result").innerHTML;
+    document.getElementById("result").innerHTML = result.substring (0, result.length -1);
+}
+
 
 function calculate () {
     
